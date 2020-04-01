f94314f1425ddb805fec6e17ac60e7aefbe1c521
Tanner Townsend <rsmcwow@gmail.com>
2020-04-01 22:43:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,9 +10,17 @@ class Dog
   def self.all
     @@all
   end
+  
+  def self.print_all
+    @@all.each { |dog| puts self[dog] }
+  end
+  
 end
 
-#fido = Dog.new("fido")
-#puppers = Dog.new("puppers")
+
+
+fido = Dog.new("fido")
+puppers = Dog.new("puppers")
 
 #p Dog.all
+p self.print_all
